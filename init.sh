ae88778dfa5f955d857eba756622e82d33ff7d55
vicg0 <victor.msantos@sptech.school>
2024-06-11 14:17:47 -0300

    pegando dados do usuário no bd local

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 # Definição de variáveis constantes para acesso ao banco de dados
 readonly USERNAME=root
+readonly SERVER=100.27.91.182
 readonly PASSWORD=urubu100
 readonly DATABASE=lifeline
 
@@ -9,11 +10,17 @@ readonly DATABASE=lifeline
 repositorio="https://github.com/LifeLine-2ADSA/scriptDocker.git"
 
 # Atualiza as listas de pacotes disponíveis e instala ferramentas necessárias
+echo "----------------Instalando pacotes e ferramentas necessárias-------------------"
 sudo apt update
 sudo apt-get update
+sudo curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/prod.list)"
+sudo apt-get update
+sudo apt-get install sqlcmd
 sudo apt-get install -y ca-certificates curl
 
 # Criação de diretório para armazenar chaves de repositório e download da chave GPG do Docker
+echo "-------------------Instalando Docker-------------------"
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -22,7 +29,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo tee /etc/apt/sources.list.d/docker.list
 
 # Atualiza novamente as listas de pacotes após adicionar o repositório do Docker
 sudo apt-get update
@@ -30,6 +37,7 @@ sudo apt-get update
 # Instalação de ferramentas Docker
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+echo "-------------------Instalando git-------------------"
 # Instalação de outras dependências
 sudo apt install -y nala git
 
@@ -41,7 +49,7 @@ sudo curl -L "https://github.com/docker/compose/releases/download/v2.10.2/docker
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Inicia o serviço do Docker, caso não esteja em execução
-sudo chmod 666 /var/run/docker.sock
+
 sudo systemctl start docker
 
 # Verifica se o Docker Daemon está em execução
@@ -52,16 +60,8 @@ while ! sudo systemctl is-active --quiet docker; do
 done
 echo "Docker iniciado com sucesso."
 
-# Derruba e inicia os contêineres usando o Docker Compose
 docker-compose down
 docker-compose up -d db
-
-# Aguarda o contêiner do banco de dados estar totalmente operacional
-echo "Aguardando o banco de dados iniciar..."
-until docker exec db_container mysqladmin ping -h "127.0.0.1" --silent; do
-    printf "."
-    sleep 2
-done
 
 # Loop para realizar o login com as credenciais fornecidas pelo usuário
 LOGIN=0
@@ -73,9 +73,9 @@ while [ "$LOGIN" -eq 0 ]; do
   echo "Digite a senha"
   read senha
 
-  # Executa uma consulta SQL no banco de dados
-  query=$(sudo docker exec db_container bash -c "MYSQL_PWD="$PASSWORD" mysql --batch -u root -D "$DATABASE" -e 'SELECT idUsuario, email, senha FROM usuario where email = \"$email\" AND senha = \"$senha\" LIMIT 1;'")
-
+#query=$(sqlcmd -S $SERVER -d $DATABASE -U $USERNAME -P $PASSWORD -Q "SELECT idUsuario, email, senha FROM usuario WHERE email = '$email' AND senha = '$senha'")
+query=$(sudo docker exec db_container bash -c "MYSQL_PWD="$PASSWORD" mysql --batch -u root -D "$DATABASE" -e 'SELECT idUsuario, email, senha FROM usuario where email = \"$email\" AND senha = \"$senha\" LIMIT 1;'")
+echo "$query"
   # Verifica se o usuário foi encontrado no banco de dados
   if [ -z "$query" ]; then
     echo "Usuário não encontrado"
